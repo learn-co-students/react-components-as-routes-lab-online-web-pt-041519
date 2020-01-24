4ade7d5bf669f0bef7d9ea82f4cbb445532b6db2
Erin Ponsonby <erinponsonby@gmail.com>
2020-01-24 09:30:06 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,15 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie =>
+        <div>
+          Title: {movie.title}
+          Time: {movie.time}
+          {movie.genres.map(genre =>
+            <ul>{genre}</ul>
+            )}
+        </div>)}
     </div>
   );
 };
