f97448ea5e6982379706028b3fe8f0187de0f6a7
Michael Chen <michaelchengaming@gmail.com>
2020-01-31 14:01:18 -0800

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink className="nav-link" to="/" exact>
+        Home
+      </NavLink>
+      <NavLink className="nav-link" to="/movies" exact>
+        Movies
+      </NavLink>
+      <NavLink className="nav-link" to="/actors" exact>
+        Actors
+      </NavLink>
+      <NavLink className="nav-link" to="/directors" exact>
+        Directors
+      </NavLink>
     </div>
   );
 };
