0f9c31d9ea84880a7b968b4b043387d72f845ec8
earthctzn <earthctzn1@gmail.com>
2020-02-03 04:21:55 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -3,10 +3,36 @@ import { actors } from '../data';
 
 const Actors = () => {
   return (
-    <div>
-      {/*{code here}*/}
-    </div>
+    <React.Fragment >
+      <h1>Actors Page</h1>
+      {actors.map(actor => {
+       return(
+              <div key={actor.name}>
+                  {actor.name} 
+                  <ul>
+                      {actor.movies.map(movie => {
+                          return( 
+                            <li key={actor.name + Math.random() * (100 - 1)}>
+                              {movie}
+                            </li>
+                          )
+                        }
+                      )
+                    }
+                  </ul>
+              </div>
+            )
+          }
+        )
+      }
+    </React.Fragment>
   );
 };
 
 export default Actors;
+
+
+// {actors.map(actor =>{
+//   return actor.movies.map(movie => {
+//   return <li>{movie}</li>})
+// })}
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -3,10 +3,30 @@ import { directors } from '../data';
 
 const Directors = () => {
   return (
-    <div>
-      {/*{code here}*/}
-    </div>
+    <React.Fragment >
+      <h1>Directors Page</h1>
+      {directors.map(director => {
+       return(
+              <div key={director.name}>
+                  {director.name} 
+                  <ul>
+                      {director.movies.map(movie => {
+                          return( 
+                            <li key={director.name + Math.random() * (100 - 1)}>
+                              {movie}
+                            </li>
+                          )
+                        }
+                      )
+                    }
+                  </ul>
+              </div>
+            )
+          }
+        )
+      }
+    </React.Fragment>
   );
-}
+};
 
 export default Directors
