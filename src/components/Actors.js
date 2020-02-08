666064438aecdb4a0e8fe1e683fcbce87044b08e
Joanna Yuhas <joannayuhas@gmail.com>
2020-02-08 16:23:22 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,18 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => {
+        return <div>
+          <h1>{actor.name}</h1>
+          <p>Movies</p>
+          <ul>
+            {actor.movies.map(movie => {
+              return <li> {movie}</li>
+            })}
+          </ul>
+          </div>
+      })}
     </div>
   );
 };
